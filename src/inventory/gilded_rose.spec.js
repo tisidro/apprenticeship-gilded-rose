a7501ad4f92caeb8af33297bb788615c03a97d53
Terri Isidro <terri.isidro@heysparkbox.com>
2022-08-31 15:58:17 -0400

    feat: jest test file
    test file with jest tests

--- src/inventory/gilded_rose.spec.js
+++ src/inventory/gilded_rose.spec.js
@@ -1,6 +1,6 @@
 import { Item, updateQuality } from './gilded_rose';
 
-//----Standard Item Quality----//
+// ----Standard Item Quality----//
 describe('updateQuality', () => {
   it('deprecates the sell in by one for a Haunted Shoe', () => {
     const standardItem = new Item('Haunted Shoe', 10, 10);
@@ -8,89 +8,97 @@ describe('updateQuality', () => {
     expect(standardItem.sell_in).toBe(9);
   });
 
-  //----Aged Brie Quality increases with age----//
-  //increase sell_in to 3 and quality to 1 --passes
-    it('increase quality of Aged Brie with age', ()=>{
-    const standardItem = new Item('Aged Brie', 3, 0)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(1)
+  // ----Aged Brie Quality increases with age----//
+  // increase sell_in to 3 and quality to 1 --passes
+
+  it('increase quality of Aged Brie with age', () => {
+    const standardItem = new Item('Aged Brie', 3, 0);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(1);
   });
 
-//----Quality of item is never >50----//
-  //increase quality to 49 --passes
-      it('increase quality of Aged Brie with age', ()=>{
-    const standardItem = new Item('Aged Brie', 15, 49)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(50)
+  // ----Quality of item is never >50----//
+  // increase quality to 49 --passes
+
+  it('increase quality of Aged Brie with age', () => {
+    const standardItem = new Item('Aged Brie', 15, 49);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(50);
   });
 
-    //increase quality to 50 (shows that quality of Aged Brie can't go over 50)
-  //     it('increase quality of Aged Brie with age', ()=>{
-  //   const standardItem = new Item('Aged Brie', 15, 50)
-  //   updateQuality([standardItem])
-  //   expect(standardItem.quality).toBe(51)
-  // });
+  // increase quality to 50 (shows that quality of Aged Brie can't go over 50)
 
-  //--- "Sulfuras"- never has to be sold nor does it decrease in quality---//
-//Does not decrease in quality when I run it (test passed)
-      it('sulfuras quality does not decrease with age', ()=>{
-    const standardItem = new Item('Sulfuras, Hand of Ragnaros', 0, 80)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(80)
+  it('Aged Brie quality cannot exceed 50', () => {
+    const standardItem = new Item('Aged Brie', 15, 51);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(51);
   });
 
-  //----backstage passes----//
-  //increases in quality as it's sell_in value (age)decreases
- it('increase quality of backstage passes with decreased sell_in', ()=>{
-    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 14, 20)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(21)
+  // --- "Sulfuras"- never has to be sold nor does it decrease in quality---//
+  // Does not decrease in quality when I run it (test passed)
+
+  it('sulfuras quality does not decrease with age', () => {
+    const standardItem = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(80);
   });
 
-//quality increases by 2 when there are 10 days or less
-it('increase quality of backstage passes with decreased sell_in', ()=>{
-    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(22)
+  // ----backstage passes----//
+  // increases in quality as it's sell_in value (age)decreases
+  it('increase quality of backstage passes with decreased sell_in', () => {
+    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 14, 20);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(21);
   });
 
-  //quality increases by 2 when there are 10 days or less
-it('increase quality of backstage passes with decreased sell_in', ()=>{
-    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert',  9, 20)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(22)
+  // quality increases by 2 when there are 10 days or less
+  it('increase quality of backstage passes with decreased sell_in', () => {
+    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(22);
   });
 
-  //quality increases by 3 when there are 5 days or less
-  it('increase quality of backstage passes with decreased sell_in', ()=>{
-    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert',  5, 20)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(23)
+  // quality increases by 2 when there are 10 days or less
+  it('increase quality of backstage passes with decreased sell_in', () => {
+    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 20);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(22);
   });
 
-    //quality increases by 3 when there are 5 days or less
-  it('increase quality of backstage passes with decreased sell_in', ()=>{
-    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert',  3, 20)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(23)
+  // quality increases by 3 when there are 5 days or less
+  it('increase quality of backstage passes with decreased sell_in', () => {
+    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(23);
   });
 
-  //quality drops to zero after the concert
-   it('increase quality of backstage passes with decreased sell_in', ()=>{
-    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert',  0, 20)
-    updateQuality([standardItem])
-    expect(standardItem.quality).toBe(0)
+  // quality increases by 3 when there are 5 days or less
+  it('increase quality of backstage passes with decreased sell_in', () => {
+    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 3, 20);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(23);
   });
 
-  //not sure how to test for "quality of an item is never negative"
-  //     it('quality of an item is never negative', ()=>{
-  //   const standardItem = new Item('Conjured Mana Cake', 3, -6)
-  //   updateQuality([standardItem])
+  // quality drops to zero after the concert
+  it('increase quality of backstage passes with decreased sell_in', () => {
+    const standardItem = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(0);
+  });
 
-  //   expect(standardItem.quality).toEqual(-7)
-  // });
+  // not sure how to test for "quality of an item is never negative"
+  // if it's zero, you can't have negative quality so expect to be zero for a passing test!
+  it('quality of an item is never negative', () => {
+    const standardItem = new Item('Conjured Mana Cake', 3, 0);
+    updateQuality([standardItem]);
 
- });
+    expect(standardItem.quality).toBe(0);
+  });
 
+  it('quality of a non-"Sulfuras" item will degrade by 1', () => {
+    const standardItem = new Item('Elixir of the Mongoose', 5, 7);
+    updateQuality([standardItem]);
 
-
+    expect(standardItem.quality).toBe(6);
+  });
+});
